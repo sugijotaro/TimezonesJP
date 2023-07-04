b0fa90ab648c33cc607ed856efe72495c418abf3
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2023-07-04 16:42:51 +0900

    Update csv_to_json.py
    fix path

--- scripts/csv_to_json.py
+++ scripts/csv_to_json.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 
 data = defaultdict(lambda: {"region": "", "region_ja": "", "cities": []})
 
-with open("../csv/timezones.csv", "r") as csvfile:
+with open("csv/timezones.csv", "r") as csvfile:
     reader = csv.DictReader(csvfile)
 
     for row in reader:
@@ -19,5 +19,5 @@ with open("../csv/timezones.csv", "r") as csvfile:
             "country_ja": row["country_ja"],
         })
 
-with open("../timezones.json", "w") as jsonfile:
+with open("timezones.json", "w") as jsonfile:
     json.dump(list(data.values()), jsonfile, ensure_ascii=False, indent=2)
